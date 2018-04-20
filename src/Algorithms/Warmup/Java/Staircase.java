13d249af9f9f043e3c98cfffbcfe3cca9fe96208
Edilson Ngulele <edilsonngulele@gmail.com>
2018-04-20 23:24:48 +0200

    Solution for the Staircase problem

---
+++ src/Algorithms/Warmup/Java/Staircase.java
@@ -0,0 +1,30 @@
+package Algorithms.Warmup.Java;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+    public static void main(String[] args) {
+
+
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (n - i - 2 < j) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+        s.close();
+
+    }
+}
